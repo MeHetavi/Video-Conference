4e6e57fa2b68d1f71bfefaa9ec889bf777a3bdb3
HetaviS <hetavi.primocys@gmail.com>
2025-05-29 14:53:52 +0530

    Changing port

--- src/config.js
+++ src/config.js
@@ -107,7 +107,7 @@ module.exports = {
       listenIps: [
         {
           ip: '0.0.0.0',
-          announcedIp: '34.100.221.31' // This will be the external IP
+          announcedIp: '35.200.163.23' // This will be the external IP
         }
       ],
       maxIncomingBitrate: 1500000,
